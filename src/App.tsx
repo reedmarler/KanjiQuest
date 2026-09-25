10276037353a0485c72404c8bbd25192440698a9
Reed <reed.marler@yahoo.com>
2026-09-24 19:39:39 -0500

    Keep intro welcome wrap stable

--- src/App.tsx
+++ src/App.tsx
@@ -493,7 +493,7 @@ function BeginnerZone({
 
     return (
       <main className="beginner-zone beginner-zone--intro">
-        <header className={`beginner-intro-guide is-${guideStep.id}`}>
+        <header className={`beginner-intro-guide is-${guideStep.id}${introOpeningShrinking ? ' is-opening-copy-swap' : ''}`}>
           <div className="beginner-intro-mascot" aria-hidden="true">
             <img src={DEFAULT_PROFILE_PHOTO} alt="" />
           </div>
